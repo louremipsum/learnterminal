0179d631555e63737f6f6dc73a1df56aa644802c
louremipsum <myself.vinayak2@gmail.com>
2024-07-24 20:17:08 +0530

    refactor: Remove unused components and imports in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
-import Header from "@/components/Header";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -23,13 +21,13 @@ export default async function Index() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <DeployButton />
+          {/* <DeployButton /> */}
           {isSupabaseConnected && <AuthButton />}
         </div>
       </nav>
 
       <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-        <Header />
+        {/* <Header /> */}
         <main className="flex-1 flex flex-col gap-6">
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
           {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
