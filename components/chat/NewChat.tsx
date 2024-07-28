888352e6fa27c4d12541ed2b0e90ff6c8af0eeec
louremipsum <myself.vinayak2@gmail.com>
2024-07-28 22:06:54 +0530

    refactor: Filter out system messages in ChatList component

--- components/chat/NewChat.tsx
+++ components/chat/NewChat.tsx
@@ -2,11 +2,18 @@ import { nanoid } from "@/lib/utils";
 import { Card, CardHeader, CardTitle, CardDescription } from "../ui/card";
 import Link from "next/link";
 
-const NewChat = () => {
+const NewChat = ({ userId }: { userId: string }) => {
   const id = nanoid();
+
   return (
-    <Link href={`/dashboard/mainframe/${id}`} prefetch={false}>
-      <Card className="rounded-lg bg-green-50 text-slate-800 w-1/2 m-auto">
+    <Link
+      href={{
+        pathname: "/dashboard/ask",
+        query: { userId: userId, chatId: id },
+      }}
+      prefetch={false}
+    >
+      <Card className="rounded-lg bg-green-50 text-slate-800 mx-auto min-w-3/4">
         <CardHeader className="drop-shadow-glow">
           <CardTitle>
             <div className="flex items-center">
