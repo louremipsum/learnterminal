6f56f81059482da2442da017fc27cf5e38642a3c
louremipsum <myself.vinayak2@gmail.com>
2024-07-28 22:05:34 +0530

    use ref for scrolling

--- components/chat/chat-scroll-anchor.tsx
+++ components/chat/chat-scroll-anchor.tsx
@@ -1,29 +1,32 @@
-'use client'
+"use client";
 
-import * as React from 'react'
-import { useInView } from 'react-intersection-observer'
-
-import { useAtBottom } from '@/lib/hooks/use-at-bottom'
+import { useInView } from "react-intersection-observer";
+import { useAtBottom } from "@/lib/hooks/use-at-bottom";
+import { useEffect } from "react";
 
 interface ChatScrollAnchorProps {
-  trackVisibility?: boolean
+  trackVisibility?: boolean;
+  refValue: React.RefObject<HTMLElement>;
 }
 
-export function ChatScrollAnchor({ trackVisibility }: ChatScrollAnchorProps) {
-  const isAtBottom = useAtBottom()
+export function ChatScrollAnchor({
+  trackVisibility,
+  refValue,
+}: ChatScrollAnchorProps) {
+  const isAtBottom = useAtBottom(refValue);
   const { ref, entry, inView } = useInView({
     trackVisibility,
     delay: 100,
-    rootMargin: '0px 0px -150px 0px'
-  })
+    root: refValue.current,
+  });
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (isAtBottom && trackVisibility && !inView) {
       entry?.target.scrollIntoView({
-        block: 'start'
-      })
+        block: "start",
+      });
     }
-  }, [inView, entry, isAtBottom, trackVisibility])
+  }, [inView, entry, isAtBottom, trackVisibility]);
 
-  return <div ref={ref} className="h-px w-full" />
+  return <div ref={ref} className="h-px w-full" />;
 }
--- components/chat/chat.tsx
+++ components/chat/chat.tsx
@@ -6,19 +6,20 @@ import { ChatPanel } from "@/components/chat/chat-panel";
 import { ChatScrollAnchor } from "@/components/chat/chat-scroll-anchor";
 import { useLocalStorage } from "@/lib/hooks/use-local-storage";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
-import { useEffect, useState } from "react";
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { toast } from "react-hot-toast";
 import Link from "next/link";
 import { ExternalLink } from "../external-link";
 import { ScrollArea } from "../ui/scroll-area";
+import { ButtonScrollToBottom } from "./button-scroll-to-bottom";
 
 export interface ChatProps extends React.ComponentProps<"div"> {
   initialMessages?: Message[];
@@ -27,10 +28,11 @@ export interface ChatProps extends React.ComponentProps<"div"> {
 
 export function Chat({ id, initialMessages, className }: ChatProps) {
   const [key, setKey] = useLocalStorage<string | null>("ai-token", null);
-  const [keyDialog, setKeyDialog] = useState<boolean>(!key);
+  const [keyAlertDialog, setKeyAlertDialog] = useState<boolean>(!key);
+  const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (key) {
-      setKeyDialog(false);
+      setKeyAlertDialog(false);
     }
   }, [key]);
   const { messages, append, reload, stop, isLoading, input, setInput } =
@@ -48,33 +50,35 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
       },
     });
   return (
-    <div className="">
-      {/* <ScrollArea className="h-screen"> */}
-      <div className={cn("pb-[100px] pt-4 md:pt-10", className)}>
-        {messages.length && (
-          <>
-            <ChatList messages={messages} />
-            <ChatScrollAnchor trackVisibility={isLoading} />
-          </>
-        )}
+    <div className={`flex flex-col h-screen ${className}`}>
+      <div className="flex-grow overflow-hidden mt-4">
+        <ScrollArea className="h-full" ref={ref}>
+          {messages.length > 0 && (
+            <>
+              <ChatList messages={messages} />
+              <ChatScrollAnchor trackVisibility={isLoading} refValue={ref} />
+            </>
+          )}
+        </ScrollArea>
       </div>
-      {/* </ScrollArea> */}
-      <ChatPanel
-        id={id}
-        isLoading={isLoading}
-        stop={stop}
-        append={append}
-        reload={reload}
-        messages={messages}
-        input={input}
-        setInput={setInput}
-      />
-
-      <Dialog open={keyDialog} onOpenChange={setKeyDialog}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Enter your OpenAI Key</DialogTitle>
-            <DialogDescription>
+      <ButtonScrollToBottom scrollRef={ref} />
+      <div className="flex justify-center items-center h-1/2">
+        <ChatPanel
+          id={id}
+          isLoading={isLoading}
+          stop={stop}
+          append={append}
+          reload={reload}
+          messages={messages}
+          input={input}
+          setInput={setInput}
+        />
+      </div>
+      <AlertDialog open={keyAlertDialog} onOpenChange={setKeyAlertDialog}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Enter your OpenAI Key</AlertDialogTitle>
+            <AlertDialogDescription>
               If you have not obtained your OpenAI API key, you can do so by{" "}
               <ExternalLink href="https://platform.openai.com/signup/">
                 signing up
@@ -82,9 +86,9 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
               on the OpenAI website. The token will be saved to your
               browser&apos;s local storage under the name{" "}
               <code className="font-mono">ai-token</code>.
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
             <Link
               href={"/dashboard/settings/ai"}
               className="text-green-500 underline"
@@ -93,9 +97,9 @@ export function Chat({ id, initialMessages, className }: ChatProps) {
                 Settings
               </Button>
             </Link>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
